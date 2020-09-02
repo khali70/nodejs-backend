c14b757c44319d88fc48a8909279eaa16e11341a
khali70 <k7alifa.a7med70@gmail.com>
2020-09-02 14:04:50 +0200

    auth and cookies 1

--- conFusionServer/app.ts
+++ conFusionServer/app.ts
@@ -12,17 +12,70 @@ import LeadersRoute from "./routes/Leaders";
 import PromoRoute from "./routes/promotion";
 
 const url = "mongodb://localhost:2020/conFusion";
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// > check the connection for any errors
+connect.then(
+  (db) => {
+    console.log("Connected correctly to the server");
+  },
+  (err) => console.log(err)
+);
+
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
+//  view the requests and loggs in the conosle
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//  the secrit key for the cookies
+app.use(cookieParser("12345-67890-09876-54321"));
+
+app.use((req, res, next) => {
+  console.log(req.signedCookies);
+  //  check if the session is saved in the cookies
+  if (!req.signedCookies.user) {
+    // > auth new user
+    let authHeader = req.headers.authorization;
+    if (!authHeader) {
+      let err = new createError("You are not authorizoted");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+    let auth = new Buffer(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    let userName = auth[0];
+    let password = auth[1];
+    if (userName == "admin" && password == "password") {
+      res.cookie("user", "admin", { signed: true });
+      next();
+    } else {
+      let err = new createError("You are not authorizoted");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+  } else {
+    // > check the valid user in the cookies
+    if (req.signedCookies.user === "admin") {
+      //  go ahead
+      next();
+    } else {
+      //  error non valid user
+      let err = new createError("You are not authorizoted");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+  }
+});
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -30,18 +83,19 @@ app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/leaders", LeadersRoute);
 app.use("/promotions", PromoRoute);
-// catch 404 and forward to error handler
+
+//  catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
+//  error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
+  //  set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // > render the error page
   res.status(err.status || 500);
   res.render("error");
 });
---
+++ conFusionServer/public/precache-manifest.d9c06998971de43d7ead6f67dc9b4287.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101340dfcbfd3def1b6114404b79fc72",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "b76d19e163ac046bbaea",
+    "url": "/confusion/static/css/2.91fbb4e8.chunk.css"
+  },
+  {
+    "revision": "b2f76b13e578a24460e1",
+    "url": "/confusion/static/css/main.bd8f6608.chunk.css"
+  },
+  {
+    "revision": "b76d19e163ac046bbaea",
+    "url": "/confusion/static/js/2.1913e0a9.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/confusion/static/js/2.1913e0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f76b13e578a24460e1",
+    "url": "/confusion/static/js/main.83c3af77.chunk.js"
+  },
+  {
+    "revision": "b75ada19d1f412b87fdd",
+    "url": "/confusion/static/js/runtime-main.31a9fb8d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
